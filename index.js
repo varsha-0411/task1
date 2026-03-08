be77e26785f36ad839bc07267549e0488c344af9
varsha-0411 <boppanavarsha411@gmail.com>
2026-03-08 22:26:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const express = require("express");
-const path = require("path");
-const wonders = require("./wonders.json");
+const express = require('express');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 80;
 
-app.use(express.static(path.join(__dirname, "../public")));
+// Serve static HTML
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.get("/api/wonders", (req, res) => {
-  res.json(wonders);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-const PORT = 80;
 
 app.listen(PORT, () => {
-  console.log(`Seven Wonders app running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
